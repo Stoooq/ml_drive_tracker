ac016dbce4daa56518dc4a11d2cf41703a8d5045
Milosz Glowacki <milosz.glow@gmail.com>
2026-04-15 13:31:08 +0200

    feat: add collate function to data loaders

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,25 +1,36 @@
+from pathlib import Path
+
 from torch.utils.data import DataLoader
 
-from dataset import Bdd100kDataset
+from src.dataset import Bdd100kDataset
+from src.utils import collate_fn, get_transforms
 
 
 class DataManager:
-    def __init__(self, root_dir: str, batch_size: int):
+    def __init__(self, root_dir: Path, batch_size: int):
         self.root_dir = root_dir
         self.batch_size = batch_size
 
     def build_dataloaders(self):
         train_dataset, val_dataset, test_dataset = self._get_data()
 
-        train_loader = DataLoader(train_dataset, self.batch_size, shuffle=True)
-        val_loader = DataLoader(val_dataset, self.batch_size, shuffle=False)
-        test_loader = DataLoader(test_dataset, self.batch_size, shuffle=False)
+        train_loader = DataLoader(train_dataset, self.batch_size, shuffle=True, collate_fn=collate_fn)
+        val_loader = DataLoader(val_dataset, self.batch_size, shuffle=False, collate_fn=collate_fn)
+        test_loader = DataLoader(test_dataset, self.batch_size, shuffle=False, collate_fn=collate_fn)
 
         return train_loader, val_loader, test_loader
 
     def _get_data(self):
-        train_dataset = Bdd100kDataset(self.root_dir, split="train")
-        val_dataset = Bdd100kDataset(self.root_dir, split="val")
-        test_dataset = Bdd100kDataset(self.root_dir, split="val")
+        augmentation_transform, transform = get_transforms(
+            dataset=None, mean=0.5, std=0.5
+        )
+
+        train_dataset = Bdd100kDataset(
+            self.root_dir,
+            split="train",
+            transform=augmentation_transform,
+        )
+        val_dataset = Bdd100kDataset(self.root_dir, split="val", transform=transform)
+        test_dataset = Bdd100kDataset(self.root_dir, split="val", transform=transform)
 
         return train_dataset, val_dataset, test_dataset
